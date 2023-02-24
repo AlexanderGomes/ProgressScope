60fb05ec8e29965b263bf7f32194fd08765ba3e8
Alexsander Gomes baptista <gomes.alexsander.hamir@outlook.com>
2023-02-24 12:22:27 -0800

    done

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -61,25 +61,3 @@ exports.loginUser = async (req, res) => {
   res.status(200).json(user);
 };
 
-exports.rateCollegue = async (req, res) => {
-  const { stars, userId, desc } = req.body;
-  const user = await User.findOne({ _id: userId });
-
-  if (!user) {
-    return res.status(400).json({ msg: "user was not found" });
-  }
-
-  try {
-    const rating = new Ratings({
-      userId: userId,
-      likes: stars,
-      desc: desc,
-    });
-
-    await rating.save();
-    res.status(200).json({ msg: "rating created" });
-  } catch (error) {
-    res.status(400).json(error.message);
-  }
-};
-
